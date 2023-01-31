c365a8d3954d91ac0c9b21dbfe527deb913283fb
DylanF <dylanbrad10@gmail.com>
2023-01-30 21:09:00 -0500

    Module export bug

--- Models/Log.js
+++ Models/Log.js
@@ -3,31 +3,40 @@ const sequelize = require("../config/connection");
 
 class Log extends Model {}
 
-Log.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  last_use: {
-    type: DataTypes.DATETIME,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "user",
-      key: "id",
+Log.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    last_use: {
+      type: DataTypes.DATETIME,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    routine_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "routine",
+        key: "id",
+      },
     },
   },
-  routine_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "routine",
-      key: "id",
-    },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "log",
   }
-});
+);
 
-module.exports = Routine;
+module.exports = Log;
