82e667e7c5295f066d69c877337692feff6572c3
DylanF <dylanbrad10@gmail.com>
2023-02-02 18:37:03 -0500

    Fixed seed bug

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-//const sequelize = require('');
+const sequelize = require('../config/connection');
 const { User, Exercise, Routine, Log } = require('../Models');
 
 const userData = require('./userData.json');
@@ -6,36 +6,36 @@ const exerciseData = require('./exerciseData.json');
 const routineData = require('./routineData.json');
 
 const seedDatabase = async () => {
-  //await sequelize.sync({ force: true });
-  // const users = await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true
-  // });
+  await sequelize.sync({ force: true });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
 
+  let numLogs = 0; 
   for (const user of userData) {
     await Log.create({
       user_id: user.id,
     });
+    numLogs++; 
   };
 
-  let routineArray = [];
-
+  let numRoutines = 0; 
   for (const routine of routineData) {
     await Routine.create({
       ...routine,
-      log_id: 1 // Dummy value
-      //routine[Math.floor(Math.random() * routines.length)].id,
+      log_id: (Math.floor(Math.random() * numLogs))
     });
+    numRoutines++; 
   };
+
 
   for (const exercise of exerciseData) {
-     
-
-    seedArray += await Exercise.create({
-      ...exercise,
-      routine_id: routine[Math.floor(Math.random() * routine.length)].id,
-    });
-  };
+    await Exercise.create({
+      ...exercise, 
+      routine_id: (Math.floor(Math.random() * numRoutines))
+    })
+  }
 };
 
 seedDatabase();
