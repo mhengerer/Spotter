7855adc18ba84e18e498113e504f562f0c91d18d
Diana <dianavw8@gmail.com>
2023-02-03 19:58:07 -0500

    adjusted placeholders for signup form, fixed references to signup form, added javascript to signup form

--- public/js/test.js
+++ public/js/test.js
@@ -1,71 +1,26 @@
-    //index.html LOGIN (button added QS to login.handlebars), NEW HERE (button href="#" id="new-user") 
+    // //index.html LOGIN (button added QS to login.handlebars), NEW HERE (button href="#" id="new-user") 
     
-    const signupFormHandler = async (event) => {
-        event.preventDefault();
+    // const signupFormHandler = async (event) => {
+    //     event.preventDefault();
       
-        const response = await fetch('/api/signup', {
-          method: 'POST',
-          body: JSON.stringify({
-            email: document.querySelector('#email-input').value,
-            password: document.querySelector('#password-input').value
-          })
-        });
+    //     const response = await fetch('/api/signup', {
+    //       method: 'POST',
+    //       body: JSON.stringify({
+    //         email: document.querySelector('#email-input').value,
+    //         password: document.querySelector('#password-input').value
+    //       })
+    //     });
       
-        if (response.ok) {
-          document.location.replace('/');
-        }
-    };
-      
-
-    document
-    .querySelector('.new-user')
-    .addEventListener('click', signupFormHandler);
+    //     if (response.ok) {
+    //       document.location.replace('/');
+    //     }
+    // };
       
 
-//page 2 SUBMIT SIGNUP FORM (button needs to be created, not on mockup)
-const form = document.querySelector('.signup-form');
-const emailInput = document.querySelector('#email-login');
-const nameInput = document.querySelector('#name-login');
-const passwordInput = document.querySelector('#password-login');
-const passwordConfirmInput = document.querySelector('#password-confirm');
-
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-   
-    const lastName = lastNameInput.value;
-    const firstName = firstNameInput.value;
-    const email = emailInput.value;
-    const password = passwordInput.value;
-    const passwordConfirm = passwordConfirmInput.value; //should this be added to the signup form?
-    const currentWeight = currentWeightInput.value;
-    const currentHeight = currentHeightInput.value;
-    const age = ageInput.value;
-
-
-    if (password !== passwordConfirm) {
-        alert('Passwords do not match');
-        return;
-    }
-
-    const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ email, password, name }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-        alert('Signup successful!');
-    } else {
-        alert('Error signing up');
-    }
-});
-
-
-
-
-
-//page 4 BACK TO WEEKLY ROUTINE (? a or button)
+    // document
+    // .querySelector('.new-user')
+    // .addEventListener('click', signupFormHandler);
+      
 
 
 
